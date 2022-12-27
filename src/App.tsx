24dd50bb56c0096c6257bcccfc758156632a609b
MiroslavKop <kopytovmiroslav@gmail.com>
2022-12-27 02:30:17 +0200

    completed homework №33

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react"
+import Title from "./components/Title"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isOpened, setOpened] = useState<boolean>(false)
+    const handleClick = () => setOpened(prevValue => !prevValue)
+    return (
+        <>
+            <div className="title">
+                {isOpened && <Title name="Hello World" type="normal"/>}
+                <button onClick={handleClick}>{isOpened ? "Close" : "Open"}</button>
+            </div>
+        </>
+    );
 }
 
 export default App;
